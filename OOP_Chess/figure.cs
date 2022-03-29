3f9ee6308f5de6e6e520cd11c3e60d9bf2a39e38
Adam Hrdý <Hrdy.Adam@stud.vassboskovice.cz>
2022-03-29 09:17:40 +0200

    Přidejte soubory projektu.

---
+++ OOP_Chess/figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Chess
+{
+    public class Figure
+    {
+        public FigureType Type { get; set; }
+        public int MyProperty { get; set; }
+        public Figure()
+    {
+
+    }
+    }
+//výčtový datový typ - seznam hodnot - interně jsou to hodnoty integer (od 0)
+    public enum FigureType
+    {
+        Pawn,  //pěšec
+        Rook,  //věž
+        Knight,//jezdec
+        Bishop,//střelec
+        Qeen,  //dáma
+        King,  //král
+
+    }
+}
